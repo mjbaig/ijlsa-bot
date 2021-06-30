b06dd5c5ed97d6db63276bc7b1be5f8f6a0253aa
Maz Baig <mjbaig@utexas.edu>
2021-06-30 00:24:30 -0500

    initial commit

---
+++ src/main/java/com/mjbaig/ijlsabot/IjlsaBotApplication.java
@@ -0,0 +1,13 @@
+package com.mjbaig.ijlsabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjlsaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IjlsaBotApplication.class, args);
+	}
+
+}
