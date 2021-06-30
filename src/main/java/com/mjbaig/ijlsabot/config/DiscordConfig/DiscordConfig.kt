86dafa207e178db25f976a042b7924d3b73679c0
Maz Baig <mjbaig@utexas.edu>
2021-06-30 01:26:21 -0500

    create discord client configuration

--- src/main/java/com/mjbaig/ijlsabot/config/DiscordConfig/DiscordConfig.kt
+++ src/main/java/com/mjbaig/ijlsabot/config/DiscordConfig/DiscordConfig.kt
@@ -1,4 +1,31 @@
 package com.mjbaig.ijlsabot.config.DiscordConfig
 
-class `class` {
+import com.mjbaig.ijlsabot.exception.NullConfigurationException
+import discord4j.core.DiscordClientBuilder
+import discord4j.core.GatewayDiscordClient
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
+import org.springframework.core.env.Environment
+
+@Configuration
+@PropertySource("classpath:secrets.properties")
+open class DiscordConfig {
+
+
+    @Bean
+    open fun discordClient(@Autowired environment: Environment): GatewayDiscordClient {
+
+        val clientId = environment.getProperty("clientId") ?: throw NullConfigurationException("client id is null")
+
+        val client = DiscordClientBuilder.create(clientId).build().login().block()
+                ?: throw NullConfigurationException("Discord Client was null")
+
+        client.onDisconnect().block()
+
+        return client
+    }
+
+
 }
---
+++ src/main/java/com/mjbaig/ijlsabot/exception/NullConfigurationException.kt
@@ -0,0 +1,6 @@
+package com.mjbaig.ijlsabot.exception
+
+import java.lang.RuntimeException
+
+class NullConfigurationException(message: String) : RuntimeException(message) {
+}
