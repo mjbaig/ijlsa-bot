9ad249777b949ca8320221ac2405e112ff62f969
Maz Baig <mjbaig@utexas.edu>
2021-06-30 19:00:56 -0500

    discord client connects

--- src/main/java/com/mjbaig/ijlsabot/config/DiscordConfig/DiscordConfig.kt
+++ src/main/java/com/mjbaig/ijlsabot/config/DiscordConfig/DiscordConfig.kt
@@ -3,24 +3,23 @@ package com.mjbaig.ijlsabot.config.DiscordConfig
 import com.mjbaig.ijlsabot.exception.NullConfigurationException
 import discord4j.core.DiscordClientBuilder
 import discord4j.core.GatewayDiscordClient
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
 import org.springframework.core.env.Environment
+import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
 @PropertySource("classpath:secrets.properties")
-open class DiscordConfig {
-
+open class DiscordConfig() {
 
     @Bean
-    open fun discordClient(@Autowired environment: Environment): GatewayDiscordClient {
+    open fun discordClient(environment: Environment, basicWebClient: WebClient): GatewayDiscordClient {
 
-        val clientId = environment.getProperty("clientId") ?: throw NullConfigurationException("client id is null")
+        val token = environment.getProperty("token") ?: throw NullConfigurationException("token is null")
 
-        val client = DiscordClientBuilder.create(clientId).build().login().block()
-                ?: throw NullConfigurationException("Discord Client was null")
+        val client = DiscordClientBuilder.create(token).build().login().block()
+                ?: throw NullConfigurationException("discord client is null")
 
         client.onDisconnect().block()
 
